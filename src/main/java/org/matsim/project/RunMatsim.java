669b370116042fa8fd71b30924859d2dd2efbe31
Robin Aurich <robin.aurich@campus.tu-berlin.de>
2021-07-02 16:21:27 +0200

    Dateien + pom + Run Klasse commit

--- src/main/java/org/matsim/project/RunMatsim.java
+++ src/main/java/org/matsim/project/RunMatsim.java
@@ -18,18 +18,28 @@
  * *********************************************************************** */
 package org.matsim.project;
 
+import java.util.Calendar;
+
 import org.matsim.api.core.v01.Scenario;
-import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.contrib.decongestion.DecongestionConfigGroup;
+import org.matsim.contrib.decongestion.DecongestionConfigGroup.DecongestionApproach;
+import org.matsim.contrib.decongestion.DecongestionConfigGroup.IntegralApproach;
+import org.matsim.contrib.decongestion.DecongestionModule;
+import org.matsim.contrib.decongestion.routing.TollTimeDistanceTravelDisutilityFactory;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.config.groups.QSimConfigGroup.SnapshotStyle;
 import org.matsim.core.config.groups.QSimConfigGroup.TrafficDynamics;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.vis.otfvis.OTFVisConfigGroup;
+
+
+/*import org.matsim.contrib.decongestion.DecongestionConfigGroup.DecongestionApproach;
+import org.matsim.contrib.decongestion.DecongestionConfigGroup.IntegralApproach;
+import org.matsim.contrib.decongestion.routing.TollTimeDistanceTravelDisutilityFactory;
+import org.matsim.core.controler.AbstractModule;*/
 
 /**
  * @author nagel
@@ -37,39 +47,90 @@ import org.matsim.vis.otfvis.OTFVisConfigGroup;
  */
 public class RunMatsim{
 
+	public final static boolean DECONGESTION = false;
+	
 	public static void main(String[] args) {
 
 		Config config;
 		if ( args==null || args.length==0 || args[0]==null ){
-			config = ConfigUtils.loadConfig( "scenarios/equil/config.xml" );
+			config = ConfigUtils.loadConfig( "scenarios/padang/config.xml" );
 		} else {
 			config = ConfigUtils.loadConfig( args );
 		}
+		
+		// add current date in format "yyyy-mm-dd-hh-mm-ss"
+					Calendar cal = Calendar.getInstance();
+					// this class counts months from 0, but days from 1
+					int month = cal.get(Calendar.MONTH) + 1;
+					String monthStr = month + "";
+					if (month < 10)
+						monthStr = "0" + month;
+					String date = cal.get(Calendar.YEAR) + "-" + monthStr + "-" + cal.get(Calendar.DAY_OF_MONTH)	+ "-" + cal.get(Calendar.HOUR_OF_DAY) + "-" + cal.get(Calendar.MINUTE) + "-" + cal.get(Calendar.SECOND);
+		
+		
+		if(DECONGESTION) {
+			// einbau der decongestion sachen
+			
+			final DecongestionConfigGroup decongestionSettings = new DecongestionConfigGroup();
+			decongestionSettings.setToleratedAverageDelaySec(30.);
+			decongestionSettings.setFractionOfIterationsToEndPriceAdjustment(1.0); //Vllt auch auf 0.8, damit sich alles stabilisert?
+			decongestionSettings.setFractionOfIterationsToStartPriceAdjustment(0.0); //nach 50% wird die Maut eingesetzt
+			decongestionSettings.setUpdatePriceInterval(1);
+			decongestionSettings.setMsa(true);
+			decongestionSettings.setTollBlendFactor(1.0);
+			
+//			decongestionSettings.setDecongestionApproach(DecongestionApproach.P_MC);
+			
+			decongestionSettings.setDecongestionApproach(DecongestionApproach.PID);
+			decongestionSettings.setKd(0);
+			decongestionSettings.setKi(0);
+			decongestionSettings.setKp(0.001);
+			decongestionSettings.setIntegralApproach(IntegralApproach.UnusedHeadway);
+			decongestionSettings.setIntegralApproachUnusedHeadwayFactor(10.0);
+			decongestionSettings.setIntegralApproachAverageAlpha(0.0);
+			decongestionSettings.setWriteOutputIteration(500);
+		
+			config.addModule(decongestionSettings);
+			
+		//Änderung Output directory
+			config.controler().setOutputDirectory("./output/"+date+"_decongestion");
+		} else {
+			config.controler().setOutputDirectory("./output/"+date+"_wodecongestion");
+		}
+		
+		
 		config.controler().setOverwriteFileSetting( OverwriteFileSetting.deleteDirectoryIfExists );
 
 		config.qsim().setTrafficDynamics( TrafficDynamics.kinematicWaves );
-		config.qsim().setSnapshotStyle( SnapshotStyle.kinematicWaves );
-
-		// possibly modify config here
+		config.qsim().setSnapshotStyle( SnapshotStyle.kinematicWaves ); //
+		config.strategy().setFractionOfIterationsToDisableInnovation(0.6); //nach x% der Iterationen Rerouting ausschalten
 		
-		// ---
 		
 		Scenario scenario = ScenarioUtils.loadScenario(config) ;
-		
-		// possibly modify scenario here
-		
-		// ---
-		
 		Controler controler = new Controler( scenario ) ;
 		
-		// possibly modify controler here
-
-		controler.addOverridingModule( new OTFVisLiveModule() ) ;
-
+		if(DECONGESTION) {
+			
+			// congestion toll computation
+			
+			controler.addOverridingModule(new DecongestionModule(scenario));
+			
+			// toll-adjusted routing
+			
+			controler.addOverridingModule(new AbstractModule(){
+				@Override
+				public void install() {
+					final TollTimeDistanceTravelDisutilityFactory travelDisutilityFactory = new TollTimeDistanceTravelDisutilityFactory();
+	                                this.bindCarTravelDisutilityFactory().toInstance( travelDisutilityFactory );
+				}
+			});
+		}
 		
-		// ---
+	
 		
 		controler.run();
+		
+	
 	}
 	
 }
